d391ba5e16b2e9d5ffb6e17090dfa56616d48ce3
Rhythmicc <RhythmLian@outlook.com>
2021-06-27 04:05:57 +0800

    first commit

--- FastCSRLoader/fcsr.h
+++ FastCSRLoader/fcsr.h
@@ -7,6 +7,7 @@ extern "C" {
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "mmio.h"
 
 #ifndef value_type
 #define value_type float
@@ -15,11 +16,11 @@ extern "C" {
 typedef struct {
     unsigned int m, n, nnz;
     unsigned int *row, *col;
-    value_type* val;
+    value_type* val, *val_im;
 } csr;
 
 // allocate a csr data structure
-csr*__new_csr(unsigned int m, unsigned int n, unsigned int nnz) {
+csr*__new_csr(unsigned int m, unsigned int n, unsigned int nnz, int isComplex, int isPattern) {
     csr* res = (csr*) malloc(sizeof(csr));
     res->m = m;
     res->n = n;
@@ -27,22 +28,48 @@ csr*__new_csr(unsigned int m, unsigned int n, unsigned int nnz) {
 
     res->row = (unsigned int*) malloc(sizeof(unsigned int) * (m + 1));
     res->col = (unsigned int*) malloc(sizeof(unsigned int) * nnz);
-    res->val = (value_type*) malloc(sizeof(value_type) * nnz);
+    if(!isPattern) res->val = (value_type*) malloc(sizeof(value_type) * nnz);
+    else res->val = NULL;
+    if (isComplex) res->val_im = (value_type*) malloc(sizeof(value_type) * nnz);
+    else res->val_im = NULL;
     return res;
 }
 
 csr* csrLoad(const char*filePath) {
     csr*res = 0;
-    
-    int fd = open(filePath, O_RDONLY), m, n, nnz;
-    if (fd < 0) return res;
-    size_t len = lseek(fd, 0, SEEK_END) ;
+    int isInteger = 0, isReal = 0, isPattern = 0, isSymmetric_tmp = 0, isComplex = 0, m, n, nnz;
+    MM_typecode matcode;
 
-    char*mbuf = (char*) mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
-    char*data = (char*) malloc(len);
-    memcpy(data, mbuf, len);
-    close(fd);
-    munmap(mbuf, len);
+    {
+        FILE*fp = fopen(filePath, "r");
+        if (fp == NULL) return res;
+
+        if (mm_read_banner(fp, &matcode) != 0) {
+            printf("Could not process Matrix Market banner.\n");
+            return res;
+        }
+        fclose(fp);
+    }
+
+    if ( mm_is_pattern( matcode ) )  isPattern = 1;
+    if ( mm_is_real ( matcode) )     isReal = 1;
+    if ( mm_is_complex( matcode ) )  isComplex = 1;
+    if ( mm_is_integer ( matcode ) ) isInteger = 1;
+    if ( mm_is_symmetric( matcode ) || mm_is_hermitian( matcode ) ) isSymmetric_tmp = 1;
+
+    char*data;
+    size_t len = 0;
+    {
+        int fd = open(filePath, O_RDONLY);
+        if (fd < 0) return res;
+        len = lseek(fd, 0, SEEK_END) ;
+
+        char*mbuf = (char*) mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
+        data = (char*) malloc(len);
+        memcpy(data, mbuf, len);
+        close(fd);
+        munmap(mbuf, len);
+    }
 
     char*line = strtok(data, "\n");
     while (line != NULL && line[0] == '%') line = strtok(NULL, "\n");
@@ -51,40 +78,71 @@ csr* csrLoad(const char*filePath) {
     unsigned int* cnt_row = (unsigned int*) calloc(m + 1, sizeof(unsigned int));
     unsigned int* ia = (unsigned int*) malloc(sizeof(unsigned int) * nnz);
     unsigned int* ja = (unsigned int*) malloc(sizeof(unsigned int) * nnz);
-    value_type* val = (value_type*) malloc(sizeof(value_type) * nnz);
+    
+    value_type*val, *val_im;
+    if (!isPattern) val = (value_type*) malloc(sizeof(value_type) * nnz);
+    if (isComplex) val_im = (value_type*) malloc(sizeof(value_type) * nnz);
 
     line = strtok(NULL, "\n");
 
     for(int index = 0; index < nnz; ++index) {
-        sscanf(line, sizeof(value_type) == sizeof(float)?"%d%d%f": "%d%d%lf", ia + index, ja + index, val + index);
-        ++cnt_row[ia[index] + 1];        
+        if (isPattern) sscanf(line, "%d%d", ia + index, ja + index);
+        else if(isReal || isInteger) sscanf(line, "%d%d%lg", ia + index, ja + index, val + index);
+        else if(isComplex) sscanf(line, "%d%d%lg%lg", ia + index, ja + index, val + index, val_im + index);
+
+        --ia[index], --ja[index];
+
+        ++cnt_row[ia[index]];        
         if((line = strtok(NULL, "\n")) == NULL && (index + 1 < nnz)) {
-            free(data), free(ia), free(ja), free(val), free(cnt_row);
+            free(data), free(ia), free(ja), free(cnt_row);
+            if (!isPattern) free(val);
+            if (isComplex) free(val_im);
             return res;
         }
     }
     
-    for (int i = 0; i < m; ++i) cnt_row[i+1] += cnt_row[i];
-    
-    res = __new_csr(m, n, nnz);
+    if (isSymmetric_tmp) for (int i = 0; i < nnz; ++i) if(ia[i] != ja[i]) ++cnt_row[ja[i]];
+    {
+        int old_val = cnt_row[0], new_val;
+        cnt_row[0] = 0;
+        for (int i = 1; i <= m; ++i) {
+            new_val = cnt_row[i];
+            cnt_row[i] = old_val + cnt_row[i-1];
+            old_val = new_val;
+        }
+    }
+
+    res = __new_csr(m, n, nnz, isComplex, isPattern);
     memcpy(res->row, cnt_row, sizeof(unsigned int) * (m + 1));
     memset(cnt_row, 0, sizeof(unsigned int) * (m + 1));
 
     for (int index = 0; index < nnz; ++index) {
         unsigned int offset = res->row[ia[index]] + cnt_row[ia[index]];
         res->col[offset] = ja[index];
-        res->val[offset] = val[index];
+        if (!isPattern) res->val[offset] = val[index];
+        if(isComplex) res->val_im[offset] = val_im[index];
         ++cnt_row[ia[index]];
+
+        if (isSymmetric_tmp) {
+            offset = res->row[ja[index]] + cnt_row[ja[index]];
+            res->col[offset] = ia[index];
+            if (!isPattern) res->val[offset] = val[index];
+            if(isComplex) res->val_im[offset] = val_im[index];
+            ++cnt_row[ja[index]];
+        }
     }
 
-    free(data), free(ia), free(ja), free(val), free(cnt_row);
+    free(data), free(ia), free(ja), free(cnt_row);
+    if (!isPattern) free(val);
+    if (isComplex) free(val_im);
     return res;
 }
 
 void free_csr(csr*csr_p) {
     free(csr_p->row);
     free(csr_p->col);
-    free(csr_p->val);
+    if(csr_p->val) free(csr_p->val);
+    if(csr_p->val_im) free(csr_p->val_im);
     free(csr_p);
 }
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char **argv) {
     if (csr*mtx = csrLoad(argv[1])) {
         for (int i = 0; i < mtx->m; ++i) printf("%d%c", mtx->row[i], i + 1 < mtx->m? '\t': '\n');
         for (int i = 0; i < mtx->nnz; ++i) printf("%d%c", mtx->col[i], i + 1 < mtx->nnz? '\t': '\n');
-        for (int i = 0; i < mtx->nnz; ++i) printf("%g%c", mtx->val[i], i + 1 < mtx->nnz? '\t': '\n');
+        if (mtx->val) for (int i = 0; i < mtx->nnz; ++i) printf("%g%c", mtx->val[i], i + 1 < mtx->nnz? '\t': '\n');
+        if (mtx->val_im) for (int i = 0; i < mtx->nnz; ++i) printf("%g%c", mtx->val_im[i], i + 1 < mtx->nnz? '\t': '\n');
         free_csr(mtx);
     }
     return 0;
